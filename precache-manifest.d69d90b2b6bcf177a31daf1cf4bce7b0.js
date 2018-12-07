f8cd0d494b7cbfe253bd8d5e55c8c4941239ede5
Matteo Azzarelli <matteus1991@gmail.com>
2018-12-07 14:53:48 +0800

    Updates

---
+++ precache-manifest.d69d90b2b6bcf177a31daf1cf4bce7b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matteoazzarelli/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "918b1d662ee99d52f4e8",
+    "url": "/matteoazzarelli/static/js/runtime~main.918b1d66.js"
+  },
+  {
+    "revision": "ee45b31ee7df6d97a081",
+    "url": "/matteoazzarelli/static/js/main.ee45b31e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/matteoazzarelli/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "ee45b31ee7df6d97a081",
+    "url": "/matteoazzarelli/static/css/main.cec2d14d.chunk.css"
+  },
+  {
+    "revision": "444e294eba9d86b515927a805b9c573d",
+    "url": "/matteoazzarelli/index.html"
+  }
+];
